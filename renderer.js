c3d0c236aa6d7a0ac3236e580ed817698b0781a1
cimorexave <sadeq.s502@gmail.com>
2023-01-28 10:19:05 +0330

    cleared; button

--- renderer.js
+++ renderer.js
@@ -6,10 +6,12 @@ const cityEl = document.getElementById('location')
 const isMap = document.getElementById('mapOption')
 const from = document.getElementById('from')
 const to = document.getElementById('to')
+const calculateBtn = document.getElementById('calculate')
+
 let cordinates
 let sunrise, sunset;
 let marker;
-isMap.addEventListener('click', () => log(isMap.value))
+
 const map = L.map('map').setView([
     35.4087524662718, 51.67968750000001], 5);
 
@@ -17,18 +19,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '',
     referPolicy: false
 }).addTo(map)
+
 map.on('click', (e) => {
     cordinates = [e.latlng.lat, e.latlng.lng]
-        if (marker){ map.removeLayer(marker); isMap.value= true}
+        if (marker) map.removeLayer(marker)
         marker = L.marker([e.latlng.lat, e.latlng.lng], {}).addTo(map)
-        .bindPopup('Your Selected Point.')
-        .openPopup()
+})
 
-    // log(from.value, to.value)
-    if (from.value && to.value) {
+calculateBtn.addEventListener('click', (e) => {
+    if (from.value && to.value && cordinates) {
         sunrise = getSunrise(cordinates[0], cordinates[1], new Date(from.value) )
         sunset = getSunset(cordinates[0], cordinates[1], new Date(from.value) )
-        log(sunrise, sunset)
-    }
-
+        log('sunrise, sunset from',sunrise, sunset)
+        sunrise = getSunrise(cordinates[0], cordinates[1], new Date(to.value) )
+        sunset = getSunset(cordinates[0], cordinates[1], new Date(to.value) )
+    } else log('no dates or location chosen')
 })
