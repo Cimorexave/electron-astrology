77f6a5bb44b8e601b47c0490a5ff4bdd84355712
Cimorexave <sadeq.s502@gmail.com>
2023-01-30 10:38:03 +0330

    toggle map and select

--- renderer.js
+++ renderer.js
@@ -13,32 +13,71 @@ let cordinates
 let sunrise, sunset;
 let marker;
 
+// toggle map function
+const toggleMap = () => {
+    log('called', isMap.checked)
+    if (isMap.checked) {
+        map.scrollWheelZoom.enable()
+        map.dragging.enable()
+        map.touchZoom.enable()
+        map.doubleClickZoom.enable()
+        map.boxZoom.enable()
+        map.keyboard.enable()
+        if (map.tap) map.tap.enable()
+    } else {
+        if (map) {
+            map.eachLayer(layer => {
+                if (layer instanceof L.Marker) map.removeLayer(marker)
+            })
+        }
+        map.scrollWheelZoom.disable()
+        map.dragging.disable()
+        map.touchZoom.disable()
+        map.doubleClickZoom.disable()
+        map.boxZoom.disable()
+        map.keyboard.disable()
+        if (map.tap) map.tap.disable()
+    }
+}
+
+// toggle map or select
+isMap.addEventListener('change', () => {
+    cityEl.toggleAttribute('disabled')
+    mapEl.classList.toggle('map-is-disabled')
+    toggleMap()
+})
+// create select options
 cities.forEach(city => {
     const option = document.createElement('option')
     option.value = [city.lat, city.lng]
     option.text = city.city
     cityEl.add(option)
 })
+// change cordinates on selecting value option
 cityEl.addEventListener('change', (e) => {
     const latlng = e.target.value.split(',')
     cordinates = [parseFloat(latlng[0]), parseFloat(latlng[1])]
     log(cordinates)
 })
-
+// creating the map
 const map = L.map('map').setView([
-    35.4087524662718, 51.67968750000001], 5);
+35.4087524662718, 51.67968750000001], 1);
+// animtaion
+map.flyTo([
+35.4087524662718, 51.67968750000001], 5)
 
+// creating the tiles
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '',
     referPolicy: false
 }).addTo(map)
-
+// selecting marker on the map
 map.on('click', (e) => {
     cordinates = [e.latlng.lat, e.latlng.lng]
         if (marker) map.removeLayer(marker)
         marker = L.marker([e.latlng.lat, e.latlng.lng], {}).addTo(map)
 })
-
+toggleMap()
 calculateBtn.addEventListener('click', (e) => {
     if (from.value && to.value && cordinates) {
         sunrise = getSunrise(cordinates[0], cordinates[1], new Date(from.value) )
